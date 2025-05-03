c8a7fc4510cdfcd48926b10164c183253010f8f8
polinavolokh <volokhpolina77@gmail.com>
2025-05-03 18:32:24 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-function openPopup() {
-    const popup = document.getElementById("popup-form");
-    popup.style.display = "block";
-  }
-  
-  function closePopup() {
-    const popup = document.getElementById("popup-form");
-    popup.style.display = "none";
-  }
-  
-  function handleFormSubmit(event) {
-    event.preventDefault();
-    alert("Ваше замовлення оформлено!");
-    closePopup();
-  }
+console.log(1323);
+
+let add = document.querySelector('.zam')
+
+add.addEventListener('click', function(){
+  let form = document.querySelector('.form-container')
+  console.log(form);
+  form.style.display = 'flex'
+  let close = document.querySelector('.add_close')
+  close.addEventListener('click', function(){
+    let form = document.querySelector('.form-container')
+    form.style.display = 'none'
+  })
+})
+
+
+let buy = document.querySelectorAll('.pr button')
+
+buy.forEach((btn)=>{
+  btn.addEventListener('click', function(){
+    let form = document.querySelector('.form-box')
+    console.log(form);
+    form.style.display = 'flex'
+    let close = document.querySelector('.new_close')
+    close.addEventListener('click', function(){
+      let form = document.querySelector('.form-box')
+      console.log(form);
+      form.style.display = 'none'
+    })
+  })
+})
+
+
+
+
